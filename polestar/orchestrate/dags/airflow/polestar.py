a0809f4745e7eae925f20400fdc44975040d54db
Peter Wang <pw33537515@gmail.com>
2026-05-08 11:26:21 +1200

    update polestar

--- polestar/orchestrate/dags/airflow/polestar.py
+++ polestar/orchestrate/dags/airflow/polestar.py
@@ -230,37 +230,73 @@ with models.DAG(
         env["DBT_BIGQUERY_PROJECT"] = 'polestar-main'
         env["DBT_BIGQUERY_DATASET"] = 'dash_table_search'
         return env
-    def set_env_vars_ga4():
-            env = get_meltano_env()
+    def set_env_vars_ga4_final():
+        env = get_meltano_env()
+        env["DBT_BIGQUERY_METHOD"] = 'oauth'
+        env["DBT_BIGQUERY_PROJECT"] = 'polestar-main'
+        env["DBT_BIGQUERY_DATASET"] = 'ga4_transformed'
+        return env
 
-            env["BQ_DATASET"] = "ga4_raw"
-            env["BQ_METHOD"] = "gcs_stage"
-            env["DBT_BIGQUERY_METHOD"] = 'oauth'
-            env["DBT_BIGQUERY_PROJECT"] = 'polestar-main'
-            env["DBT_BIGQUERY_DATASET"] = 'ga4_transformed'       
-            developer_creds = Credentials(
-                None,
-                refresh_token=env["TAP_GA4_OAUTH_CREDENTIALS_REFRESH_TOKEN"],
-                token_uri="https://oauth2.googleapis.com/token",
-                client_id=env["TAP_GA4_OAUTH_CREDENTIALS_CLIENT_ID"],
-                client_secret=env["TAP_GA4_OAUTH_CREDENTIALS_CLIENT_SECRET"],
-            )
-            developer_creds.refresh(Request())
-            env["TAP_GA4_OAUTH_CREDENTIALS_ACCESS_TOKEN"] = developer_creds.token
-            env["TAP_GA4_START_DATE"] = get_ga4_start_date()
-            return env
-        
-    kube_ga4 = KubernetesPodOperator(
-            name="polestar-ga4-to-bigquery",
-            task_id="polestar-ga4_to_bigquery",
+    def set_env_vars_ga4(goal):
+        env = get_meltano_env()
+        if goal == "session":
+            env["GA4_REPORTS"] = "./report_sessions.json"
+            env["GA4_GOAL"] = "session_goal"
+        else:
+            env["GA4_REPORTS"] = "./report.json"
+            env["GA4_GOAL"] = "goal"
+        env["BQ_DATASET"] = "ga4_raw"
+        env["BQ_METHOD"] = "gcs_stage"
+        env["DBT_BIGQUERY_METHOD"] = 'oauth'
+        env["DBT_BIGQUERY_PROJECT"] = 'polestar-main'
+        env["DBT_BIGQUERY_DATASET"] = 'ga4_transformed'
+        developer_creds = Credentials(
+            None,
+            refresh_token=env["TAP_GA4_OAUTH_CREDENTIALS_REFRESH_TOKEN"],
+            token_uri="https://oauth2.googleapis.com/token",
+            client_id=env["TAP_GA4_OAUTH_CREDENTIALS_CLIENT_ID"],
+            client_secret=env["TAP_GA4_OAUTH_CREDENTIALS_CLIENT_SECRET"],
+        )
+        developer_creds.refresh(Request())
+        env["TAP_GA4_OAUTH_CREDENTIALS_ACCESS_TOKEN"] = developer_creds.token
+        env["TAP_GA4_START_DATE"] = get_ga4_start_date()
+        return env
+
+    kube_ga4_final = KubernetesPodOperator(
+            name="polestar-ga4-final-to-bigquery",
+            task_id="polestar-ga4_final_to_bigquery",
             namespace="composer-user-workloads",
             image=IMAGE,
-            arguments=["--environment=prod", "run", "tap-ga4", "target-bigquery","dbt-bigquery:ga4_models"],
+            arguments=["--environment=prod", "invoke", "dbt-bigquery:ga4_final_models"],
             container_resources=k8s_models.V1ResourceRequirements(
                 limits={"memory": "1000M", "cpu": "500m"},
             ),
-            env_vars=set_env_vars_ga4(),
-        ) 
+            env_vars=set_env_vars_ga4_final(),
+            get_logs=True,
+        )
+
+    kube_ga4_list = []
+    ga4_goals = ["goal", "session"]
+    for goal in ga4_goals:
+        kube_ga4_t = KubernetesPodOperator(
+                name=f"polestar-{goal}-ga4-to-bigquery",
+                task_id=f"polestar-{goal}-ga4_to_bigquery",
+                namespace="composer-user-workloads",
+                image=IMAGE,
+                arguments=[
+                    "--environment=prod",
+                    "run",
+                    "tap-ga4",
+                    "target-bigquery",
+                    f"dbt-bigquery:ga4_{goal}_models",
+                ],
+                container_resources=k8s_models.V1ResourceRequirements(
+                    limits={"memory": "1000M", "cpu": "500m"},
+                ),
+                env_vars=set_env_vars_ga4(goal=goal),
+                get_logs=True,
+            )
+        kube_ga4_list.append(kube_ga4_t)
 
    
    
@@ -327,4 +363,6 @@ with models.DAG(
   
 
     [kube_google_ads_search] >> kube_dash
-    kube_dash>>kube_dash_search >> kube_dash_union >> kube_ga4
+    kube_dash >> kube_dash_search >> kube_dash_union
+    for task in kube_ga4_list:
+        kube_dash_union >> task >> kube_ga4_final
