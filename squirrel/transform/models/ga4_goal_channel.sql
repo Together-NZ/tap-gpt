2017bce547a06dc6ac04c2e2926809d15f5b69bc
Peter Wang <pw33537515@gmail.com>
2026-05-07 12:45:54 +1200

    merge sessio and goal data from ga4 for squirrel

--- squirrel/transform/models/ga4_goal_channel.sql
+++ squirrel/transform/models/ga4_goal_channel.sql
@@ -1,7 +1,9 @@
-{{ config(
-    materialized='table',
-) }}
-{{ ga4.ga4_goal_channel(source_name='dash_union', table_name='dash_union', plan_code=env_var('PLAN_CODE_GA4', 'squ'), ga4_goal_a_model='ga4_goal_a') }}
+{{
+    config(
+        materialized='table',
+    )
+}}
+{{ ga4.ga4_goal_channel_final(ga4_session_source_name='ga4_session_transformed', ga4_session_table_name='ga4_goal_channel_session', ga4_goal_source_name='ga4_goal_transformed', ga4_goal_table_name='ga4_goal_channel_goal') }}
 
 
 
