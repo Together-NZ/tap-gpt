90357e3eebb7e9597b5ebfcd268155658ecad5aa
Peter Wang <pw33537515@gmail.com>
2026-05-14 18:33:25 +1200

    remove session seperation on brand level for ga4 data, merge session and goal data together before brand seperation

--- kiwibank/orchestrate/dags/airflow/kiwibank.py
+++ kiwibank/orchestrate/dags/airflow/kiwibank.py
@@ -438,6 +438,19 @@ with models.DAG(
             env_vars=set_env_vars_ga4_overall(goal),
         )
         ga4_task_list.append(kube_ga4_overall)
+    kube_ga4_final = KubernetesPodOperator(
+        name="kb-ga4-final-to-bq",
+        task_id="kb-ga4_final_to_bigquery",
+        namespace="composer-user-workloads",
+        image=IMAGE,
+        arguments=["--environment=prod", "invoke", "dbt-bigquery:ga4_final_models"],
+        container_resources=k8s_models.V1ResourceRequirements(
+            limits={"memory": "1000M", "cpu": "500m"},
+        ),
+        env_vars=set_env_vars_ga4_overall(),
+    )
+    for task in ga4_task_list:
+        task >> kube_ga4_final
     kube_tiktok = KubernetesPodOperator(
         name="kb-tiktok-to-bq",
         task_id="kb-tiktok_to_bigquery",
@@ -481,28 +494,14 @@ with models.DAG(
             namespace="composer-user-workloads",
             image=IMAGE,
             arguments=["--environment=prod", "invoke",
-                        f"dbt-bigquery:ga4_{brand}_goal_models"],
+                        f"dbt-bigquery:ga4_{brand}_models"],
             container_resources=k8s_models.V1ResourceRequirements(
                 limits={"memory": "1000M", "cpu": "500m"},
             ),
             env_vars=set_env_vars_ga4_brand(brand),
         )
-        kube_ga4_brand_session = KubernetesPodOperator(
-            name=f"kb-{brand}-ga4-channel-session-to-bq",
-            task_id=f"kb-{brand}-ga4_channel_session_to_bigquery",
-            namespace="composer-user-workloads",
-            image=IMAGE,
-            arguments=["--environment=prod", "invoke",
-                        f"dbt-bigquery:ga4_{brand}_session_models"],
-            container_resources=k8s_models.V1ResourceRequirements(
-                limits={"memory": "1000M", "cpu": "500m"},
-            ),
-            env_vars=set_env_vars_ga4_brand(brand),
-        )
-        for task in ga4_task_list:
-            kube_dash_overall>>task >> kube_ga4_brand
-            kube_dash_overall>>task >> kube_ga4_brand_session
-
+ 
+        kube_dash_overall>> kube_ga4_final >> kube_ga4_brand
 
         kube_google_ads = KubernetesPodOperator(
                 name=f"kb-{brand}-google-ads-to-bq",
@@ -552,5 +551,5 @@ with models.DAG(
             ),
             env_vars=set_env_vars_dash(brand),
         )
-        for task in ga4_task_list:
-            [kube_google_ads,kube_tiktok] >> kube_dash >> kube_dash_search >> kube_dash_union >> task
+        
+        [kube_google_ads,kube_tiktok] >> kube_dash >> kube_dash_search >> kube_dash_union >> kube_ga4_final >> kube_ga4_brand
