8a0c9cbcf6255e0fac7ae1ffc21e33114587c7f9
Peter Wang <pw33537515@gmail.com>
2026-05-19 12:08:24 +1200

    upload Cupra

---
+++ cupra/orchestrate/dags/airflow/cupra.py
@@ -0,0 +1,336 @@
+import datetime
+from copy import deepcopy
+from datetime import timedelta
+
+import pendulum
+from airflow import models
+from airflow.providers.cncf.kubernetes.operators.pod import KubernetesPodOperator
+from airflow.models import Variable
+from airflow.sensors.external_task import ExternalTaskSensor
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from kubernetes.client import models as k8s_models
+
+IMAGE = "australia-southeast1-docker.pkg.dev/cupra-main/meltano/meltano-cupra-main:prod"
+PROJECT_NAME = "cupra-main"
+
+local_tz = pendulum.timezone("Pacific/Auckland")
+
+default_args = {
+    "retries": 3,
+    "max_active_runs": 1,
+    "concurrency": 1,
+    "catchup": False,
+    "retry_delay": timedelta(minutes=30),
+    "start_date": datetime.datetime(2025, 1, 1, tzinfo=local_tz),
+}
+
+
+def get_meltano_env():
+    meltano_env_unique = Variable.get("meltano_cupra_main", deserialize_json=True)
+    meltano_env_common = Variable.get("meltano_common_secret", deserialize_json=True)
+    meltano_env = {**meltano_env_common, **meltano_env_unique}
+    start_date_str = (
+        datetime.datetime.now(local_tz) - datetime.timedelta(days=14)
+    ).strftime("%Y-%m-%d")
+    meltano_env["START_DATE"] = start_date_str
+    meltano_env["BQ_METHOD"] = "batch_job"
+    return deepcopy(meltano_env)
+
+
+def get_ga4_start_date():
+    return (datetime.datetime.now(local_tz) - datetime.timedelta(days=30)).strftime("%Y-%m-%d")
+
+
+def set_env_vars_facebook():
+    env = get_meltano_env()
+    env["BQ_DATASET"] = "facebook_raw"
+    env["BQ_METHOD"] = "batch_job"
+    env["DBT_BIGQUERY_METHOD"] = "oauth"
+    env["DBT_BIGQUERY_PROJECT"] = PROJECT_NAME
+    env["DBT_BIGQUERY_DATASET"] = "facebook_transformed"
+    return env
+
+
+def set_env_vars_cm360():
+    env = get_meltano_env()
+    env["DBT_BIGQUERY_METHOD"] = "oauth"
+    env["DBT_BIGQUERY_PROJECT"] = PROJECT_NAME
+    env["DBT_BIGQUERY_DATASET"] = "cm360_transformed"
+    return env
+
+
+def set_env_vars_dv360():
+    env = get_meltano_env()
+    env["BQ_DATASET"] = "dv360_raw"
+    env["BQ_METHOD"] = "batch_job"
+    env["DBT_BIGQUERY_METHOD"] = "oauth"
+    env["DBT_BIGQUERY_PROJECT"] = PROJECT_NAME
+    env["DBT_BIGQUERY_DATASET"] = "dv360_transformed"
+    return env
+
+def set_env_vars_dash():
+        env = get_meltano_env()
+        env["DBT_BIGQUERY_METHOD"] = "oauth"
+        env["DBT_BIGQUERY_PROJECT"] = PROJECT_NAME
+        env["DBT_BIGQUERY_DATASET"] = "dash_table"
+        return env
+
+
+with models.DAG(
+    dag_id="cupra-meltano-extraction-transformation-dbt",
+    schedule_interval="0 14 * * *",
+    default_args=default_args,
+) as dag:
+    kube_facebook = KubernetesPodOperator(
+        name="cupra-facebook-to-bigquery",
+        task_id="cupra-facebook_to_bigquery",
+        namespace="composer-user-workloads",
+        image=IMAGE,
+        arguments=[
+            "--environment=prod",
+            "run",
+            "tap-facebook",
+            "target-bigquery",
+            "dbt-bigquery:facebook_models",
+        ],
+        container_resources=k8s_models.V1ResourceRequirements(
+            limits={"memory": "1000M", "cpu": "500m"},
+        ),
+        env_vars=set_env_vars_facebook(),
+        get_logs=True,
+    )
+
+    kube_cm360 = KubernetesPodOperator(
+        name="cupra-cm360-to-bigquery",
+        task_id="cupra-cm360_to_bigquery",
+        namespace="composer-user-workloads",
+        image=IMAGE,
+        arguments=["--environment=prod", "invoke", "dbt-bigquery:cm360_models"],
+        container_resources=k8s_models.V1ResourceRequirements(
+            limits={"memory": "1000M", "cpu": "500m"},
+        ),
+        env_vars=set_env_vars_cm360(),
+        get_logs=True,
+    )
+
+    kube_dv360 = KubernetesPodOperator(
+        name="cupra-dv360-to-bigquery",
+        task_id="cupra-dv360_to_bigquery",
+        namespace="composer-user-workloads",
+        image=IMAGE,
+        arguments=[
+            "--environment=prod",
+            "run",
+            "tap-dv360",
+            "target-bigquery",
+            "dbt-bigquery:dv360_models",
+        ],
+        container_resources=k8s_models.V1ResourceRequirements(
+            limits={"memory": "1000M", "cpu": "500m"},
+        ),
+        env_vars=set_env_vars_dv360(),
+        get_logs=True,
+    )
+    kube_dash = KubernetesPodOperator(
+        name="cupra-dash-to-bigquery",
+        task_id="cupra-dash_to_bigquery",
+        namespace="composer-user-workloads",
+        image=IMAGE,
+        trigger_rule="all_done",
+        arguments=[
+            "--environment=prod",
+            "invoke",
+            "dbt-bigquery",
+            "run",
+            "--select",
+            "dash_table",
+        ],
+        container_resources=k8s_models.V1ResourceRequirements(
+            limits={"memory": "1000M", "cpu": "500m"},
+        ),
+        env_vars=set_env_vars_dash(),
+        get_logs=True,
+    )
+    kube_dash_union = KubernetesPodOperator(
+        name="cupra-dash-union-to-bigquery",
+        task_id="cupra-dash_union_to_bigquery",
+        namespace="composer-user-workloads",
+        image=IMAGE,
+        arguments=[
+            "--environment=prod",
+            "invoke",
+            "dbt-bigquery",
+            "run",
+            "--select",
+            "dash_union",
+        ],
+        container_resources=k8s_models.V1ResourceRequirements(
+            limits={"memory": "1000M", "cpu": "500m"},
+        ),
+        env_vars=set_env_vars_dash(),
+        get_logs=True,
+    )
+
+
+
+
+    kube_cm360 >> kube_dv360
+    [kube_dv360,kube_facebook] >> kube_dash
+    kube_dash >> kube_dash_union
+
+
+with models.DAG(
+    dag_id="cupra-meltano-ga4",
+    schedule_interval="30 15 * * *",
+    default_args=default_args,
+) as ga4_dag:
+
+    def set_env_vars_dash():
+        env = get_meltano_env()
+        env["DBT_BIGQUERY_METHOD"] = "oauth"
+        env["DBT_BIGQUERY_PROJECT"] = PROJECT_NAME
+        env["DBT_BIGQUERY_DATASET"] = "dash_table"
+        return env
+
+    def set_env_vars_dash_search():
+        env = get_meltano_env()
+        env["DBT_BIGQUERY_METHOD"] = "oauth"
+        env["DBT_BIGQUERY_PROJECT"] = PROJECT_NAME
+        env["DBT_BIGQUERY_DATASET"] = "dash_table_search"
+        return env
+
+    def set_env_vars_ga4(goal):
+        env = get_meltano_env()
+        if goal == "session":
+            env["TAP_GA4_REPORTS"] = "./report_sessions.json"
+            env["GA4_GOAL"] = "session_goal"
+        else:
+            env["TAP_GA4_REPORTS"] = "./report.json"
+            env["GA4_GOAL"] = "goal"
+        env["BQ_DATASET"] = "ga4_raw"
+        env["BQ_METHOD"] = "gcs_stage"
+        env["DBT_BIGQUERY_METHOD"] = "oauth"
+        env["DBT_BIGQUERY_PROJECT"] = PROJECT_NAME
+        env["DBT_BIGQUERY_AUTH_METHOD"] = "oauth"
+        env["DBT_BIGQUERY_DATASET"] = "ga4_transformed"
+        developer_creds = Credentials(
+            None,
+            refresh_token=env["TAP_GA4_OAUTH_CREDENTIALS_REFRESH_TOKEN"],
+            token_uri="https://oauth2.googleapis.com/token",
+            client_id=env["TAP_GA4_OAUTH_CREDENTIALS_CLIENT_ID"],
+            client_secret=env["TAP_GA4_OAUTH_CREDENTIALS_CLIENT_SECRET"],
+        )
+        developer_creds.refresh(Request())
+        env["TAP_GA4_START_DATE"] = get_ga4_start_date()
+        env["TAP_GA4_OAUTH_CREDENTIALS_ACCESS_TOKEN"] = developer_creds.token
+        return env
+
+    def set_env_vars_ga4_final():
+        env = get_meltano_env()
+        env["DBT_BIGQUERY_METHOD"] = "oauth"
+        env["DBT_BIGQUERY_PROJECT"] = PROJECT_NAME
+        env["DBT_BIGQUERY_DATASET"] = "ga4_transformed"
+        return env
+
+
+
+    kube_dash = KubernetesPodOperator(
+        name="cupra-dash-to-bigquery",
+        task_id="cupra-dash_to_bigquery",
+        namespace="composer-user-workloads",
+        image=IMAGE,
+        trigger_rule="all_done",
+        arguments=[
+            "--environment=prod",
+            "invoke",
+            "dbt-bigquery",
+            "run",
+            "--select",
+            "dash_table",
+        ],
+        container_resources=k8s_models.V1ResourceRequirements(
+            limits={"memory": "1000M", "cpu": "500m"},
+        ),
+        env_vars=set_env_vars_dash(),
+        get_logs=True,
+    )
+
+    kube_dash_search = KubernetesPodOperator(
+        name="cupra-dash-search-to-bigquery",
+        task_id="cupra-dash_search_to_bigquery",
+        namespace="composer-user-workloads",
+        image=IMAGE,
+        arguments=[
+            "--environment=prod",
+            "invoke",
+            "dbt-bigquery",
+            "run",
+            "--select",
+            "dash_table_search",
+        ],
+        container_resources=k8s_models.V1ResourceRequirements(
+            limits={"memory": "1000M", "cpu": "500m"},
+        ),
+        env_vars=set_env_vars_dash_search(),
+        get_logs=True,
+    )
+
+    kube_dash_union = KubernetesPodOperator(
+        name="cupra-dash-union-to-bigquery",
+        task_id="cupra-dash_union_to_bigquery",
+        namespace="composer-user-workloads",
+        image=IMAGE,
+        arguments=[
+            "--environment=prod",
+            "invoke",
+            "dbt-bigquery",
+            "run",
+            "--select",
+            "dash_union",
+        ],
+        container_resources=k8s_models.V1ResourceRequirements(
+            limits={"memory": "1000M", "cpu": "500m"},
+        ),
+        env_vars=set_env_vars_dash(),
+        get_logs=True,
+    )
+
+    kube_ga4_final = KubernetesPodOperator(
+        name="cupra-ga4-final-to-bigquery",
+        task_id="cupra-ga4_final_to_bigquery",
+        namespace="composer-user-workloads",
+        image=IMAGE,
+        arguments=["--environment=prod", "invoke", "dbt-bigquery:ga4_final_models"],
+        container_resources=k8s_models.V1ResourceRequirements(
+            limits={"memory": "1000M", "cpu": "500m"},
+        ),
+        env_vars=set_env_vars_ga4_final(),
+        get_logs=True,
+    )
+
+    kube_ga4_list = []
+    for goal in ("goal", "session"):
+        kube_ga4 = KubernetesPodOperator(
+            name=f"cupra-ga4-{goal}-to-bigquery",
+            task_id=f"cupra-ga4_{goal}_to_bigquery",
+            namespace="composer-user-workloads",
+            image=IMAGE,
+            arguments=[
+                "--environment=prod",
+                "run",
+                "tap-ga4",
+                "target-bigquery",
+                f"dbt-bigquery:ga4_{goal}_models",
+            ],
+            container_resources=k8s_models.V1ResourceRequirements(
+                limits={"memory": "1000M", "cpu": "500m"},
+            ),
+            env_vars=set_env_vars_ga4(goal),
+            get_logs=True,
+        )
+        kube_ga4_list.append(kube_ga4)
+
+    kube_dash >> kube_dash_search >> kube_dash_union
+    for kube_ga4 in kube_ga4_list:
+        kube_dash_union >> kube_ga4 >> kube_ga4_final
---
+++ cupra/transform/models/dash_table.sql
@@ -0,0 +1,24 @@
+{{ config(
+    materialized='table',
+) }}
+
+WITH dash_table AS (
+    {{ dash_table_general_process.dv360_standard(source_name='dv360_transformed', table_name='dv360_standard', yt_source_name='dv360_transformed', yt_table_name='dv360_youtube') }}
+    UNION ALL
+    {{ dash_table_general_process.dv360_youtube(source_name='dv360_transformed', table_name='dv360_youtube') }}
+    UNION ALL
+    {{ dash_table_general_process.google_ads(source_name='google_ads', table_name='google_ads_demand') }}
+    UNION ALL
+    {{ dash_table_general_process.meta(source_name='facebook_transformed', table_name='facebook') }}
+    UNION ALL
+    {{ dash_table_general_process.cm360(source_name='cm360_transformed', table_name='cm360_direct_buy') }}
+),
+with_channel AS (
+    SELECT * EXCEPT (publisher, channel),
+        dc.publisher,
+        dc.channel
+    FROM dash_table AS dt
+    JOIN `together-internal.channel.publisher_channel` AS dc
+        ON LOWER(TRIM(dt.publisher)) = LOWER(TRIM(dc.publisher))
+),
+{{ dash_table_general_process.dash_table_general_process() }}
