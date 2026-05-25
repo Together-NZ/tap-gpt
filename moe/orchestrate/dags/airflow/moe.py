caf8455c3f9ad5df8dda75bf427d4e49172caa36
Peter Wang <pw33537515@gmail.com>
2026-05-26 09:13:07 +1200

    testing runtime with 30 days in MOE

--- moe/orchestrate/dags/airflow/moe.py
+++ moe/orchestrate/dags/airflow/moe.py
@@ -48,7 +48,7 @@ def get_meltano_env():
     meltano_env_unique = Variable.get("meltano_moe_main", deserialize_json=True)
     meltano_env_common = Variable.get("meltano_common_secret",deserialize_json=True)
     meltano_env = {**meltano_env_common, **meltano_env_unique}
-    yesterday = datetime.datetime.now(local_tz) - datetime.timedelta(days=13)
+    yesterday = datetime.datetime.now(local_tz) - datetime.timedelta(days=30)
     start_date_str = yesterday.strftime("%Y-%m-%d")
 
     meltano_env["START_DATE"] = start_date_str
