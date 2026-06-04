1c5895af87cbd21fbb48238101ec7f2d0421e211
Peter Wang <pw33537515@gmail.com>
2026-06-04 17:07:56 +1200

    add ga4 secret

--- wcet/orchestrate/dags/airflow/wcet.py
+++ wcet/orchestrate/dags/airflow/wcet.py
@@ -34,7 +34,8 @@ def get_ga4_start_date():
 def get_meltano_env():
     meltano_env_unique = Variable.get("meltano_wcet_main", deserialize_json=True)
     meltano_env_common = Variable.get("meltano_common_developer_main", deserialize_json=True)
-    meltano_env = {**meltano_env_common, **meltano_env_unique}
+    meltano_env_ga4 = Variable.get("meltano_developer_ga4_main", deserialize_json=True)
+    meltano_env = {**meltano_env_common, **meltano_env_unique, **meltano_env_ga4}
     meltano_env["START_DATE"] = (
         datetime.datetime.now(local_tz) - datetime.timedelta(days=14)
     ).strftime("%Y-%m-%d")
