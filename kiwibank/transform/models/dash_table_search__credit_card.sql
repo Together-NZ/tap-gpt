cd3cf69db9dd4e79de41f53caaff383f9e024a26
Peter Wang <pw33537515@gmail.com>
2026-06-17 10:07:09 +1200

    fix dash table search for brands to rename segments devices to device

--- kiwibank/transform/models/dash_table_search__credit_card.sql
+++ kiwibank/transform/models/dash_table_search__credit_card.sql
@@ -2,6 +2,6 @@
     materialized='table',
 ) }}
 with dash_table AS (
-SELECT * FROM `kiwibank-main.google_ads_search_transformed__credit_card.google_ads_search__credit_card`
+SELECT SUM(conversions) AS conversions, date,SUM(media_cost) AS media_cost,SUM(clicks) AS clicks, segments_device as device, SUM(impressions) AS impressions,campaign_name,campaign_id,publisher FROM `kiwibank-main.google_ads_search_transformed__credit_card.google_ads_search__credit_card` GROUP BY campaign_name,campaign_id,device,publisher,date
 )
 {{ dash_table_general_process.dash_table_search_general_process('Choose') }}
