771496e387ee87cad1b13da9c8c8a3d0bc7b73c0
Peter Wang <pw33537515@gmail.com>
2026-06-22 10:47:04 +1200

    fix dash table search transformation rule for both brands

--- wcet/transform/models/dash_table_search__wop.sql
+++ wcet/transform/models/dash_table_search__wop.sql
@@ -7,4 +7,4 @@ with dash_table AS (
   SELECT SUM(conversions) AS conversions, date,SUM(media_cost) AS media_cost,SUM(clicks) AS clicks, segments_device as device, SUM(impressions) AS impressions,campaign_name,campaign_id,publisher FROM `wcet-main.google_ads_search_transformed__wop.google_ads_search__wop` GROUP BY campaign_name,campaign_id,device,publisher,date
 )
 )
-{{ dash_table_general_process.dash_table_search_general_process('Choose') }}
+{{ dash_table_general_process.dash_table_search_general_process('INTENT') }}
