669cdea7437cc5bbc159f08cc4afcaab13d5338e
Peter Wang <pw33537515@gmail.com>
2026-07-14 10:50:35 +1200

    update dash table search for wendys and zeekr

--- wendys/transform/models/dash_table_search.sql
+++ wendys/transform/models/dash_table_search.sql
@@ -1,20 +1,7 @@
 {{ config(
     materialized='table',
 ) }}
-
-WITH dash_table AS (
-    SELECT
-        SUM(conversions) AS conversions,
-        date,
-        SUM(media_cost) AS media_cost,
-        SUM(clicks) AS clicks,
-        segments_device AS device,
-        SUM(impressions) AS impressions,
-        campaign_name,
-        campaign_id,
-        publisher
-    FROM {{ ref('google_ads_search') }}
-    GROUP BY campaign_name, campaign_id, device, publisher, date
+with dash_table AS (
+SELECT SUM(conversions) AS conversions, date, SUM(media_cost) AS media_cost, SUM(clicks) AS clicks, segments_device as device, SUM(impressions) AS impressions, campaign_name, campaign_id, publisher FROM `wendys-main.google_ads_search_transformed.google_ads_search` GROUP BY campaign_name, campaign_id, device, publisher, date
 )
-
 {{ dash_table_general_process.dash_table_search_general_process('INTENT') }}
