aa34532a587f267d1866f28036869599292f8efa
Peter Wang <pw33537515@gmail.com>
2026-07-13 16:31:27 +1200

    fix ao social boosting dash table

--- kiwibank/transform/models/dash_table__ao_social_boosting.sql
+++ kiwibank/transform/models/dash_table__ao_social_boosting.sql
@@ -5,22 +5,22 @@
     materialized='table',
 ) }}
 WITH dash_table AS (
-{{dash_table_general_process.tiktok(source_name='tiktok_transformed', table_name='tiktok')          }} WHERE campaign_name LIKE '%00011%'
+{{dash_table_general_process.tiktok(source_name='tiktok_transformed', table_name='tiktok')}} WHERE (campaign_name LIKE '%00011%' or campaign_name like '%000026%')
 UNION ALL
-{{dash_table_general_process.meta(source_name='facebook_transformed', table_name='facebook')}} WHERE campaign_name LIKE '%00011%'
+{{dash_table_general_process.meta(source_name='facebook_transformed', table_name='facebook')}} WHERE (campaign_name LIKE '%00011%' or campaign_name like '%000026%')
 UNION ALL
-{{dash_table_general_process.linkedin(source_name='linkedin_transformed', table_name='linkedin')}} WHERE campaign_name LIKE '%00011%'
+{{dash_table_general_process.linkedin(source_name='linkedin_transformed', table_name='linkedin')}} WHERE (campaign_name LIKE '%00011%' or campaign_name like '%000026%')
 UNION ALL
-{{dash_table_general_process.dv360_standard(source_name='dv360_transformed', table_name='dv360_standard',yt_source_name='dv360_transformed',yt_table_name='dv360_youtube')}} AND campaign_name LIKE '%00011%'
+{{dash_table_general_process.dv360_standard(source_name='dv360_transformed', table_name='dv360_standard',yt_source_name='dv360_transformed',yt_table_name='dv360_youtube')}} AND (campaign_name LIKE '%00011%' or campaign_name like '%000026%')
 UNION ALL
-{{dash_table_general_process.dv360_youtube(source_name='dv360_transformed', table_name='dv360_youtube')}} WHERE campaign_name LIKE '%00011%'
+{{dash_table_general_process.dv360_youtube(source_name='dv360_transformed', table_name='dv360_youtube')}} WHERE (campaign_name LIKE '%00011%' or campaign_name like '%000026%')
 UNION ALL
-{{dash_table_general_process.hivestack(source_name='hivestack_transformed', table_name='hivestack')}} WHERE campaign_name LIKE '%00011%'
+{{dash_table_general_process.hivestack(source_name='hivestack_transformed', table_name='hivestack')}} WHERE (campaign_name LIKE '%00011%' or campaign_name like '%000026%')
 UNION ALL
-{{dash_table_general_process.cm360(source_name='cm360_transformed', table_name='cm360_direct_buy')}} WHERE campaign_name LIKE '%00011%'
+{{dash_table_general_process.cm360(source_name='cm360_transformed', table_name='cm360_direct_buy')}} WHERE (campaign_name LIKE '%00011%' or campaign_name like '%000026%')
 UNION ALL
 {{dash_table_general_process.ttd(source_name='ttd_transformed', table_name='ttd')}} WHERE (campaign_name LIKE '%00011%' or campaign_name like '%000026%'
-),
+)),
 with_channel AS (
 SELECT * EXCEPT (publisher, channel),
 dc.publisher,
