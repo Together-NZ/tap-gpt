5214f22e159dd7704a9604f7d306c1292168d1fb
Peter Wang <pw33537515@gmail.com>
2026-07-15 16:29:20 +1200

    remove keyword ga

--- uow/orchestrate/dags/airflow/uow.py
+++ uow/orchestrate/dags/airflow/uow.py
@@ -221,7 +221,7 @@ with models.DAG(
         ),
         env_vars=set_env_vars_ga4_final(),
     )
-    goal_list = ['goal','session','keyword']
+    goal_list = ['goal','session']
     kube_ga4_list = []
     for goal in goal_list:
         kube_ga4 = KubernetesPodOperator(
