12d1a2624b6611e9d5cc68f1cd7833af8a66bc83
Peter Wang <pw33537515@gmail.com>
2026-07-21 11:46:19 +1200

    modified dash table search into dbt package for aia

--- aia/transform/models/dash_table_search.sql
+++ aia/transform/models/dash_table_search.sql
@@ -3,35 +3,15 @@
 ) }}
 
 WITH dash_table AS (
-    (
-        SELECT
-            SUM(conversions) AS conversions,
-            date,
-            SUM(media_cost) AS media_cost,
-            SUM(clicks) AS clicks,
-            segments_device AS device,
-            SUM(impressions) AS impressions,
-            campaign_name,
-            campaign_id,
-            publisher
-        FROM {{ source('google_ads_search_transformed__brand', 'google_ads_search_brand') }}
-        GROUP BY campaign_name, campaign_id, device, publisher, date
-    )
+    {{ dash_table_general_process.google_ads_search(
+        source_name='google_ads_search_transformed__brand',
+        table_name='google_ads_search_brand'
+    ) }}
     UNION ALL
-    (
-        SELECT
-            SUM(conversions) AS conversions,
-            date,
-            SUM(media_cost) AS media_cost,
-            SUM(clicks) AS clicks,
-            segments_device AS device,
-            SUM(impressions) AS impressions,
-            campaign_name,
-            campaign_id,
-            publisher
-        FROM {{ source('google_ads_search_transformed__marketing', 'google_ads_search_marketing') }}
-        GROUP BY campaign_name, campaign_id, device, publisher, date
-    )
+    {{ dash_table_general_process.google_ads_search(
+        source_name='google_ads_search_transformed__marketing',
+        table_name='google_ads_search_marketing'
+    ) }}
 )
 
 {{ dash_table_general_process.dash_table_search_general_process('INTENT') }}
