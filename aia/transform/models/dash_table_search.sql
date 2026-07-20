283b1e778b2bb3b602defcc4098b8a3d8672dc3c
Peter Wang <pw33537515@gmail.com>
2026-07-20 17:05:59 +1200

    update aia with outbrain as dbt package

--- aia/transform/models/dash_table_search.sql
+++ aia/transform/models/dash_table_search.sql
@@ -1,48 +1,37 @@
 {{ config(
     materialized='table',
 ) }}
-with dash_table AS (
+
+WITH dash_table AS (
     (
         SELECT
-            * EXCEPT(conversions),
-            conversions AS metrics_conversions,
-            CASE WHEN LOWER(publisher) != 'demand gen' THEN campaign_name END AS campaign_name_selection
-        FROM `aia-nz-main.google_ads_search_transformed__brand.google_ads_search_brand`
+            SUM(conversions) AS conversions,
+            date,
+            SUM(media_cost) AS media_cost,
+            SUM(clicks) AS clicks,
+            segments_device AS device,
+            SUM(impressions) AS impressions,
+            campaign_name,
+            campaign_id,
+            publisher
+        FROM {{ source('google_ads_search_transformed__brand', 'google_ads_search_brand') }}
+        GROUP BY campaign_name, campaign_id, device, publisher, date
     )
     UNION ALL
     (
         SELECT
-            * EXCEPT(conversions),
-            conversions AS metrics_conversions,
-            CASE WHEN LOWER(publisher) != 'demand gen' THEN campaign_name END AS campaign_name_selection
-        FROM `aia-nz-main.google_ads_search_transformed__marketing.google_ads_search_marketing`
+            SUM(conversions) AS conversions,
+            date,
+            SUM(media_cost) AS media_cost,
+            SUM(clicks) AS clicks,
+            segments_device AS device,
+            SUM(impressions) AS impressions,
+            campaign_name,
+            campaign_id,
+            publisher
+        FROM {{ source('google_ads_search_transformed__marketing', 'google_ads_search_marketing') }}
+        GROUP BY campaign_name, campaign_id, device, publisher, date
     )
-),
-funnels as (
-    select distinct funnel from `aia-nz-main.dash_table.dash_table`
 )
-SELECT *
-,
-CASE WHEN 
- 
-       EXISTS(SELECT 1 FROM UNNEST(SPLIT(campaign_name,'_'))  as a
-       WHERE lower(a) in UNNEST(ARRAY['aud','disp','native','pdooh','rmdisp','social','vid','vidod','yt']))
-       THEN  (SELECT X FROM UNNEST(SPLIT(campaign_name,'_') ) as X WHERE lower(X) IN UNNEST(['aud','disp','native','pdooh','rmdisp','social','vid','vidod','yt'])
-       LIMIT 1)
-       else 'Other'
-END as media_format,
-'Paid Search' as channel,
-NULL AS creative_name,
-NULL AS ad_format, 
-CASE WHEN 'INTENT' IN (select distinct funnel from `aia-nz-main.dash_table.dash_table`) then 'INTENT'
-ELSE 'Other' END as funnel,
-NULL AS ad_format_detail,
-NULL AS audience_name,
-NULL AS video_completion,
-NULL AS video_50_completion,
-NULL AS video_25_completion,
-NULL AS video_75_completion,
-null as video_views,
-null as campaign_descr,
-null as creative_descr
-from dash_table
+
+{{ dash_table_general_process.dash_table_search_general_process('INTENT') }}
