ba902682bee5b1088a9056807e1af9ca9f3cd50b
Peter Wang <pw33537515@gmail.com>
2026-07-21 11:35:19 +1200

    update source inputation in dash table search with dbt packages, add platform in the final output in dash union

--- kiwibank/transform/models/dash_table_search__business_banking.sql
+++ kiwibank/transform/models/dash_table_search__business_banking.sql
@@ -2,6 +2,6 @@
     materialized='table',
 ) }}
 with dash_table AS (
-SELECT SUM(conversions) AS conversions, date,SUM(media_cost) AS media_cost,SUM(clicks) AS clicks, segments_device as device, SUM(impressions) AS impressions,campaign_name,campaign_id,publisher FROM `kiwibank-main.google_ads_search_transformed__business_banking.google_ads_search__business_banking` GROUP BY campaign_name,campaign_id,device,publisher,date
+{{dash_table_general_process.google_ads_search(source_name='google_ads__business_banking', table_name='google_ads_search__business_banking')}}
 )
 {{ dash_table_general_process.dash_table_search_general_process('Choose') }}
--- kiwibank/transform/models/dash_table_search__everyday_banking_retail_deposit.sql
+++ kiwibank/transform/models/dash_table_search__everyday_banking_retail_deposit.sql
@@ -2,15 +2,10 @@
     materialized='table',
 ) }}
 with dash_table AS (
- (
-  SELECT conversions, date,media_cost,clicks,device,impressions,campaign_name,campaign_id,publisher FROM `kiwibank-main.google_ads_search_transformed__everyday_banking_retail_deposit.bing_ads_search__everyday_banking_retail_deposit`
-) UNION ALL 
-(
-  SELECT SUM(conversions) AS conversions, date,SUM(media_cost) AS media_cost,SUM(clicks) AS clicks, segments_device as device, SUM(impressions) AS impressions,campaign_name,campaign_id,publisher FROM `kiwibank-main.google_ads_search_transformed__everyday_banking_retail_deposit.google_ads_search__everyday_banking_retail_deposit1` GROUP BY campaign_name,campaign_id,device,publisher,date
-)
-UNION ALL 
-(
-  SELECT SUM(conversions) AS conversions, date,SUM(media_cost) AS media_cost,SUM(clicks) AS clicks, segments_device as device, SUM(impressions) AS impressions,campaign_name,campaign_id,publisher FROM `kiwibank-main.google_ads_search_transformed__everyday_banking_retail_deposit.google_ads_search__everyday_banking_retail_deposit2` GROUP BY campaign_name,campaign_id,device,publisher,date
-)
+{{dash_table_general_process.google_ads_search(source_name='google_ads__everyday_banking_retail_deposit', table_name='google_ads_search__everyday_banking_retail_deposit1')}}
+UNION ALL
+{{dash_table_general_process.google_ads_search(source_name='google_ads__everyday_banking_retail_deposit', table_name='google_ads_search__everyday_banking_retail_deposit2')}}
+UNION ALL
+{{dash_table_general_process.bing_ads_search(source_name='google_ads__everyday_banking_retail_deposit', table_name='bing_ads_search__everyday_banking_retail_deposit')}}
 )
 {{ dash_table_general_process.dash_table_search_general_process('Choose') }}
