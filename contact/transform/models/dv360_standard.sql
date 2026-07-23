59f5a05685e8fc6169f386baa2b191175620c8cc
Peter Wang <pw33537515@gmail.com>
2026-07-23 17:55:22 +1200

    update contact energy dbt packages, modified task execution order, add platform into final tables

---
+++ contact/transform/models/dv360_standard.sql
@@ -0,0 +1,9 @@
+{{ config(
+    materialized='table',
+) }}
+{{ dv360.dv360_standard(
+    source_name='dv360_raw',
+    table_name='dv360_standard',
+    cm360_source_name='cm360_transformed',
+    cm360_table_name='cm360_direct_buy'
+) }}
---
+++ contact/transform/models/hivestack.sql
@@ -0,0 +1,7 @@
+{{ config(
+    materialized='table',
+) }}
+{{ hivestack.hivestack(
+    table_name='hivestack_raw',
+    report_name=env_var('REPORT_NAME', 'contact_report')
+) }}
