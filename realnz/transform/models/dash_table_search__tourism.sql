0fa298f5da98427d07383f268701fc926d8b490a
Peter Wang <pw33537515@gmail.com>
2026-07-28 16:03:25 +1200

    modified dash table search and ga4 transformation rule on realnz

--- realnz/transform/models/dash_table_search__tourism.sql
+++ realnz/transform/models/dash_table_search__tourism.sql
@@ -4,33 +4,15 @@
 ) }}
 
 WITH dash_table AS (
-    (
-        SELECT
-            conversions,
-            date,
-            media_cost,
-            clicks,
-            device,
-            impressions,
-            campaign_name,
-            campaign_id,
-            publisher
-        FROM {{ source('google_ads_search_transformed__tourism', 'bing_ads_search__tourism') }}
-    )
+    {{ dash_table_general_process.bing_ads_search(
+        source_name='google_ads_search_transformed__tourism',
+        table_name='bing_ads_search__tourism'
+    ) }}
     UNION ALL
-    (
-        SELECT
-            SUM(conversions) AS conversions,
-            date,
-            SUM(media_cost) AS media_cost,
-            SUM(clicks) AS clicks,
-            segments_device AS device,
-            SUM(impressions) AS impressions,
-            campaign_name,
-            campaign_id,
-            publisher
-        FROM {{ source('google_ads_search_transformed__tourism', 'google_ads_search__tourism') }}
-        GROUP BY campaign_name, campaign_id, device, publisher, date
-    )
+    {{ dash_table_general_process.google_ads_search(
+        source_name='google_ads_search_transformed__tourism',
+        table_name='google_ads_search__tourism'
+    ) }}
 )
+
 {{ dash_table_general_process.dash_table_search_general_process('BOOK') }}
