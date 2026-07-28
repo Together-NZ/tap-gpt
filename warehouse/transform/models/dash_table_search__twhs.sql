c885af1e8c18b48830ec1a1728b507bb7a35ceb6
Peter Wang <pw33537515@gmail.com>
2026-07-28 12:58:58 +1200

    implement dbt package

--- warehouse/transform/models/dash_table_search__twhs.sql
+++ warehouse/transform/models/dash_table_search__twhs.sql
@@ -5,35 +5,15 @@
 ) }}
 
 WITH dash_table AS (
-    (
-        SELECT
-            SUM(conversions) AS conversions,
-            date,
-            SUM(media_cost) AS media_cost,
-            SUM(clicks) AS clicks,
-            device,
-            SUM(impressions) AS impressions,
-            campaign_name,
-            campaign_id,
-            publisher
-        FROM {{ ref('bing_ads_search__twhs') }}
-        GROUP BY campaign_name, campaign_id, device, publisher, date
-    )
+    {{ dash_table_general_process.bing_ads_search(
+        source_name='google_ads__twhs',
+        table_name='bing_ads_search__twhs'
+    ) }}
     UNION ALL
-    (
-        SELECT
-            SUM(conversions) AS conversions,
-            date,
-            SUM(media_cost) AS media_cost,
-            SUM(clicks) AS clicks,
-            segments_device AS device,
-            SUM(impressions) AS impressions,
-            campaign_name,
-            campaign_id,
-            publisher
-        FROM {{ ref('google_ads_search__twhs') }}
-        GROUP BY campaign_name, campaign_id, device, publisher, date
-    )
+    {{ dash_table_general_process.google_ads_search(
+        source_name='google_ads__twhs',
+        table_name='google_ads_search__twhs'
+    ) }}
 )
 
 {{ dash_table_general_process.dash_table_search_general_process('INTENT') }}
