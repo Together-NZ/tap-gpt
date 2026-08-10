bfe71e60116a7882a4c70c113e5eacbbeeac30b2
Peter Wang <pw33537515@gmail.com>
2026-08-10 12:37:57 +1200

    update farizon

--- farizon/transform/models/ga4_goal_channel_keyword.sql
+++ farizon/transform/models/ga4_goal_channel_keyword.sql
@@ -3,9 +3,9 @@
     incremental_strategy='insert_overwrite',
     partition_by={'field': 'date', 'data_type': 'date'},
 ) }}
-{{ ga4.ga4_goal_a(
+{{ ga4.ga4_goal_channel_keyword(
     source_name='ga4_raw',
-    table_name='goal',
+    table_name='keyword_goal',
     dash_union_source_name='dash_union',
     dash_union_table_name='dash_union'
 ) }}
