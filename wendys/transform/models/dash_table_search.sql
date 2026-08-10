d146dd1782f002a75f473e6d53ddc94e76b85f91
Peter Wang <pw33537515@gmail.com>
2026-08-11 09:59:17 +1200

    add dash table search dbt package

--- wendys/transform/models/dash_table_search.sql
+++ wendys/transform/models/dash_table_search.sql
@@ -1,7 +1,11 @@
 {{ config(
     materialized='table',
 ) }}
-with dash_table AS (
-SELECT SUM(conversions) AS conversions, date, SUM(media_cost) AS media_cost, SUM(clicks) AS clicks, segments_device as device, SUM(impressions) AS impressions, campaign_name, campaign_id, publisher FROM `wendys-main.google_ads_search_transformed.google_ads_search` GROUP BY campaign_name, campaign_id, device, publisher, date
+
+WITH dash_table AS (
+    {{ dash_table_general_process.google_ads_search(
+        source_name='google_ads',
+        table_name='google_ads_search'
+    ) }}
 )
 {{ dash_table_general_process.dash_table_search_general_process('INTENT') }}
