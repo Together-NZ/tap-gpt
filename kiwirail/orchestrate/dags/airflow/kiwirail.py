8e6bf79082abfbf4e448d7cb1deeea86a70cbf48
Peter Wang <pw33537515@gmail.com>
2026-08-10 16:43:39 +1200

    add ttd task comparison

--- kiwirail/orchestrate/dags/airflow/kiwirail.py
+++ kiwirail/orchestrate/dags/airflow/kiwirail.py
@@ -624,6 +624,34 @@ with models.DAG(
         get_logs=True,
     )
 
+    def make_ttd_comparison(b):
+        def ttd_comparison_check(**context):
+            env = get_meltano_env()
+            trigger = ComparisonTrigger(
+                project_name=PROJECT_NAME,
+                destination_table=f"ttd_transformed__{b}",
+                table_name=f"ttd_transformed__{b}",
+                source_name="ttd",
+                start_date=comparison_start_date,
+                end_date=datetime.datetime.now(local_tz).strftime("%Y-%m-%d"),
+                secret_name=COMPARISON_SECRET,
+                project_id=env["PROJECT_ID"],
+                brand=b,
+            )
+            result = trigger.compare_data()
+            if not result:
+                raise ValueError(f"TTD data accuracy check failed for {b}.")
+            return result
+
+        return ttd_comparison_check
+
+    task_ttd_comparison = PythonOperator(
+        task_id=f"task_ttd_comparison_{brand}",
+        python_callable=make_ttd_comparison(brand),
+        retries=0,
+        trigger_rule="all_done",
+    )
+
     kube_dv360_freight = KubernetesPodOperator(
         name="kiwirail-freight-dv360-to-bigquery",
         task_id="kiwirail-freight-dv360_to_bigquery",
@@ -755,6 +783,7 @@ with models.DAG(
 
     kube_linkedin >> task_linkedin_comparison
     kube_cm360_freight >> [kube_ttd, kube_dv360_freight]
+    kube_ttd >> task_ttd_comparison
     kube_dv360_freight >> [
         task_dv360_freight_standard_comparison,
         task_dv360_freight_youtube_comparison,
