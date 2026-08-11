54bda3b60a9ee781273942cf01785d7c5c924b2f
Peter Wang <pw33537515@gmail.com>
2026-08-12 01:12:02 +1200

    remove bing for noel leeming

--- warehouse/transform/models/bing_ads_search__noel_leeming.sql
+++
@@ -1,7 +0,0 @@
-{{ config(
-    materialized='table',
-    schema='google_ads_search_transformed__noel_leeming',
-    alias='bing_ads_search__noel_leeming',
-) }}
-
-{{ google_ads.bing_ads_search_warehouse(client_id=7068886290) }}
