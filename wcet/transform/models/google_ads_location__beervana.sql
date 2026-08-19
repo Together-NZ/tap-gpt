1ea1e9053a87609feabf25f4527e8f002eaa79d9
Peter Wang <pw33537515@gmail.com>
2026-08-20 11:54:25 +1200

    add location

---
+++ wcet/transform/models/google_ads_location__beervana.sql
@@ -0,0 +1,9 @@
+{{ config(
+    materialized='table',
+    schema='google_ads_search_transformed__volvo',
+    alias='google_ads_location__volvo',
+    partition_by={'field': 'date', 'data_type': 'date'},
+    cluster_by=['campaign_name_selection', 'channel', 'funnel', 'publisher'],
+) }}
+
+{{ google_ads.google_ads_search_location(client_id=7965466558) }}
