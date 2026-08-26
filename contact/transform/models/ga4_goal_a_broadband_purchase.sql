c8cf8c0361672e2c2f343413dc002afa9b2089cb
Peter Wang <pw33537515@gmail.com>
2026-08-26 15:08:11 +1200

    change the dash union name on broadband and energy purchase

--- contact/transform/models/ga4_goal_a_broadband_purchase.sql
+++ contact/transform/models/ga4_goal_a_broadband_purchase.sql
@@ -6,6 +6,6 @@
 {{ ga4.ga4_goal_a_contact_purchase(
     source_name='ga4_raw',
     table_name='contact_purchase_broadband_report',
-    dash_union_source_name='dash_union_broadband',
-    dash_union_table_name='dash_union__broadband'
+    dash_union_source_name='dash_table',
+    dash_union_table_name='dash_union'
 ) }}
