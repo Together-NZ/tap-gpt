8f5e3449fbf4359691762efd8fd60e66dc52ad3c
Peter Wang <pw33537515@gmail.com>
2026-09-02 14:27:06 +1200

    update contact energy google ads update to incremental, update funnel filter

--- contact/transform/models/bing_ads_search.sql
+++ contact/transform/models/bing_ads_search.sql
@@ -1,4 +1,8 @@
 {{ config(
     materialized='table',
 ) }}
-{{ google_ads.bing_ads_search_sa360(client_id=6806887247) }}
+{{ google_ads.bing_ads_search_sa360(
+    client_id=6806887247,
+    sa360_dataset='sa360_contact',
+    transfer_id=4291961021,
+) }}
